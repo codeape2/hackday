effb89beb45e7518a5365c2a3cf58ad156eed181
Bernt R. Brenna <bernt.brenna@gmail.com>
2018-06-27 14:26:49 +0200

    implemented left right etc

--- roverfront/src/modules/rover.ts
+++ roverfront/src/modules/rover.ts
@@ -10,12 +10,20 @@ export default class Rover implements IRover {
         this.connection = new RoverConnection(roverSettings.host);
     }
 
-    public async forward(): Promise<void> {
-        await this.connection.execute("forward", {});
+    public async forward(seconds:number=0, speed:number=1.0): Promise<void> {
+        await this.connection.execute("forward", {seconds: seconds, speed: speed});
     }
 
     public async stop(): Promise<void> {
         await this.connection.execute("stop", {});
+    }
+
+    public async right(seconds:number=0, speed:number=0.5): Promise<void> {
+        await this.connection.execute("right", {seconds: seconds, speed: speed})
+    }
+
+    public async left(seconds:number=0, speed:number=0.5): Promise<void> {
+        await this.connection.execute("left", {seconds: seconds, speed: speed})
     }
 
     public async wait(waitInMs: number): Promise<void> {
